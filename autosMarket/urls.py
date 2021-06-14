a6523cba4c916458d4c22cb487f654c139c33e3e
Alvaro Lagos <a.lagosd@alumnos.duoc.cl>
2021-06-13 21:59:49 -0400

    Creacion de Templates HTML del Sitio, enlaces en urls y views, carga imagenes album

--- autosMarket/urls.py
+++ autosMarket/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, albumAutos, autosNuevos, autosUsados, contacto, vendeauto
 #importa la vista desde template el home
 
 urlpatterns = [
     path('', home, name="home"),
+    path('albumAutos/', albumAutos, name="albumAutos"),
+    path('autosNuevos/', autosNuevos, name="autosNuevos"),
+    path('autosUsados/', autosUsados, name="autosUsados"),
+    path('contacto/', contacto, name="contacto"),
+    path('vendeauto/', vendeauto, name="vendeauto"),
 ]
--- autosMarket/views.py
+++ autosMarket/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
-#Conectan el modelo con el programa de
+#Crea una funcion que retorna la vista de la url que se declara
 
 def home(request):
     return render(request, 'autosMarket/home.html')
+
+    
+def albumAutos(request):
+    return render(request, 'autosMarket/albumAutos.html')
+
+
+def autosNuevos(request):
+    return render(request, 'autosMarket/autosNuevos.html')
+
+    
+def autosUsados(request):
+    return render(request, 'autosMarket/autosUsados.html')
+
+    
+def contacto(request):
+    return render(request, 'autosMarket/contacto.html')
+
+    
+def vendeauto(request):
+    return render(request, 'autosMarket/vendeauto.html')
