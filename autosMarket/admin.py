7f74f7d9ad6f4b1fa76aa56f865b478193a6f24c
Alvaro Lagos <a.lagosd@alumnos.duoc.cl>
2021-06-14 19:11:24 -0400

    Sqlite y Admin

--- autosMarket/admin.py
+++ autosMarket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Marca, Producto 
 
 # Register your models here.
+#otorgar acceso a Las clases marca y producto al dashboard 
+admin.site.register(Marca)
+admin.site.register(Producto)
+
--- autosMarket/models.py
+++ autosMarket/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    #funcion que devuelve el campo nombre como string     
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField() 
+    nuevo = models.BooleanField()
+    # clave foranea de la clase marca, en el caso que se elimine una marca los productos no quedaran sin marca 
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateField()
+    def __str__(self):
+        return self.nombre
