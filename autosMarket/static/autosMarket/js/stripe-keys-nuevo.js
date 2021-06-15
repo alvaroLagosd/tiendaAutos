9c34649656d5f5ed51961d9699f5b50093236a62
Alvaro Lagos <a.lagosd@alumnos.duoc.cl>
2021-06-14 22:30:50 -0400

    modificacion archivos js

---
+++ autosMarket/static/autosMarket/js/stripe-keys-nuevo.js
@@ -0,0 +1,4 @@
+export default {
+    public:"pk_test_51ImmePBjm79XRSQxCDIywcRGZBeFvF98ZCrrnQcUuEoLysyEpmalPxWDfugmhXDhcd1aNDgN7ooU9DA8s4ARSt4U00QTJCLmVL",
+    secret:"sk_test_51ImmePBjm79XRSQxB2iDBHTmJNyRkNZmQWZ5jgepDvHLle4TriUKBmVOy0MFHDvG1oP0K1rxvIBnEWkaNsWz8Kew00MVg6QzTH"
+}
---
+++ autosMarket/static/autosMarket/js/stripe-keys.js
@@ -0,0 +1,5 @@
+//Exportando Objeto con clave pública y clave secreta de la API de Stripe
+export default {
+    public: "pk_test_51ImNB4Kn8CdVNFBVLUjRJ4ouBrfXj8haLXUjjedsTxlQOaXKtL1UMvHVIw7S352vAWvMUfMI6GP35aMHkUAEA14S00QQA53Sdk",
+    secret:"sk_test_51ImNB4Kn8CdVNFBV0y06qgQrZBUUgaGU1DkyueZ6LNukoHqkDle9C9rtjxEsvwSF7Jwjx2hsqxYUgSDKplgz8fYf006vPMNLwC"
+}
