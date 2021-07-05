45daa86b70a6645845eb8b46b23a8718e96f69f5
Alvaro Lagos <a.lagosd@alumnos.duoc.cl>
2021-07-05 16:35:57 -0400

    Creacion API REST

--- autosMarket/urls.py
+++ autosMarket/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import home, albumAutos, autosNuevos, autosUsados, contacto, vendeauto, registro
+from django.urls import path, include
+from .views import home, albumAutos, autosNuevos, autosUsados, contacto,\
+     vendeauto, registro, ProductoViewset
 #importa la vista desde template el home
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('producto', ProductoViewset)
 
 
 urlpatterns = [
@@ -13,4 +18,5 @@ urlpatterns = [
     path('vendeauto/', vendeauto, name="vendeauto"),
     path('config/', views.stripe_config),
     path('registro/', registro, name="registro"),  # new
+    path('api/', include(router.urls))
 ]
